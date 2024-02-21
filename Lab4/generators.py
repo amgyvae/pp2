d0f244157f43a9b2dd3e579e0d71fa01b671c89c
Margulan Sharipzhan <142485675+amgyvae@users.noreply.github.com>
2024-02-21 09:37:13 +0600

    Update generators.py

--- Lab4/generators.py
+++ Lab4/generators.py
@@ -7,3 +7,13 @@ n = int(input())
 cntSquare = funkSquare(n)
 for i in cntSquare:
     print(i)
+
+#2nd task
+def funkEven(n):
+    for i in range(n + 1):
+        if i % 2 == 0:
+            yield i
+
+n = int(input())
+cntEven = funkEven(n)
+print(*cntEven, sep=", ")
