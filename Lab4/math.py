2b879154ac27ac84441a21764c52fa26f34001ad
Margulan Sharipzhan <142485675+amgyvae@users.noreply.github.com>
2024-02-21 09:46:36 +0600

    Update math.py

--- Lab4/math.py
+++ Lab4/math.py
@@ -9,9 +9,17 @@ print("Output radian:", radian)
 h = 5
 b1 = 5
 b2 = 6
-
 S = 0.5 * (b1 + b2) * h
 print("Height:", h)
 print("Base, first value:", b1)
 print("Base, second value:", b2)
 print("Expected Output:", S)
+
+#3rd task
+import math
+s = 4
+l = 25
+Sp = (s * l ** 2) / (4 * math.tan(math.pi / s))
+print("Input number of sides:", s)
+print("Input the length of a side:", l)
+print("The area of the polygon is:", Sp)
