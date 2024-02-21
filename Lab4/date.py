13d66f53e0555c3edb1133c2edf6f0b491d56b88
Margulan Sharipzhan <142485675+amgyvae@users.noreply.github.com>
2024-02-21 09:28:56 +0600

    Update date.py

--- Lab4/date.py
+++ Lab4/date.py
@@ -1,3 +1,4 @@
+#1st task
 from datetime import datetime, timedelta
 
 current_date = datetime.now()
@@ -6,3 +7,15 @@ five_days_ago = current_date - timedelta(days=5)
 
 print("Current Date:", current_date)
 print("Five Days Ago:", five_days_ago)
+
+#2nd task
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+
+yesterday = current_date - timedelta(days=1)
+tomorrow = current_date + timedelta(days=1)
+
+print("Yesterday:", yesterday)
+print("Today:", current_date)
+print("Tomorrow:", tomorrow)
