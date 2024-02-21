94f435e6bdf2740d9509a00461aa0f760ab35f02
Margulan Sharipzhan <142485675+amgyvae@users.noreply.github.com>
2024-02-21 09:45:26 +0600

    Update math.py

--- Lab4/math.py
+++ Lab4/math.py
@@ -6,3 +6,12 @@ print("Input degree:", m)
 print("Output radian:", radian)
 
 #2nd task
+h = 5
+b1 = 5
+b2 = 6
+
+S = 0.5 * (b1 + b2) * h
+print("Height:", h)
+print("Base, first value:", b1)
+print("Base, second value:", b2)
+print("Expected Output:", S)
